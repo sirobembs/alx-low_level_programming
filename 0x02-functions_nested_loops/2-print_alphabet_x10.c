fa89dda4ba8a4c2177999c9c16419e21faf38031
sirobembs <danielobembe1@gmail.com>
2022-03-15 08:55:19 -0700

    wrote a program that prints 10 times the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - wrote a program that prints 10 times the alphabet in lower case
+ * print_alphabet_x10 - wrote a program that prints 10 times the alphabet in lower case
  *
  * Return: Always 0 (success)
  */
